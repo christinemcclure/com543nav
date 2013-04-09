c57fbace819bc46b9be44390a6db7e7235e9e0aa
Christine McClure <camcclure@gmail.com>
2013-04-09 16:03:22 -0500

    copied stripped-down version of sample page on project site

---
+++ index.php
@@ -0,0 +1,92 @@
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width" />
+<title>Sample Page | June 24, 2012</title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="http://project.printobject.com/xmlrpc.php" />
+<!--[if lt IE 9]>
+<script src="http://project.printobject.com/wp-content/themes/book-lite/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+
+<link rel='stylesheet' id='admin-bar-css'  href='http://project.printobject.com/wp-includes/css/admin-bar.min.css?ver=3.5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='content-editable-style-css'  href='http://project.printobject.com/wp-content/plugins/wpcontenteditable/content-editable-style.css?ver=3.5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='tpicker-css'  href='http://project.printobject.com/wp-content/plugins/taxonomy-picker/taxonomy-picker.css?ver=1302062028' type='text/css' media='all' />
+<link rel='stylesheet' id='style-css'  href='http://project.printobject.com/wp-content/themes/book-lite-child/style.css?ver=3.5.1' type='text/css' media='all' />
+<script type='text/javascript' src='http://project.printobject.com/wp-includes/js/jquery/jquery.js?ver=1.8.3'></script>
+<style type="text/css" media="print">#wpadminbar { display:none; }</style>
+<style type="text/css" media="screen">
+	html { margin-top: 28px !important; }
+	* html body { margin-top: 28px !important; }
+</style>
+	<style type="text/css">
+			.site-title a,
+		.site-description {
+			color: #111;
+		}
+		/* .site-title a {
+			text-shadow: -1px -1px 0 #333,
+    			1px -1px 0 #333,
+    			-1px 1px 0 #333,
+     			1px 1px 0 #333;
+		} */
+		.site-title a:hover {
+			opacity: 0.7;
+		}
+
+
+	</style>
+	</head>
+
+<body class="page page-id-2 page-template-default logged-in admin-bar no-customize-support no-custom-header no-home">
+<div id="page" class="hfeed site">
+		<header id="masthead" class="site-header" role="banner">
+		<nav role="navigation" class="site-navigation main-navigation">
+			<h1 class="assistive-text">Menu</h1>
+			<div class="assistive-text skip-link"><a href="#main" title="Skip to content">Skip to content</a></div>
+
+			<div id="main-menu" class="menu-simple-menu-container"><ul id="menu-simple-menu" class="menu"><li id="menu-item-526" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-526"><a href="http://project.printobject.com"><span class="contenteditable title" data-pid="2"  ><span class="saver">Home</span></span></a></li>
+<li id="menu-item-530" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-530"><a href="http://project.printobject.com/about/"><span class="contenteditable title" data-pid="2"  ><span class="saver">About</span></span></a></li>
+</ul></div>		</nav>
+
+		<hgroup>
+			<h1 class="site-title"><a href="http://project.printobject.com/" title="June 24, 2012" rel="home">June 24, 2012</a></h1>
+			<h2 class="site-description"></h2>
+		</hgroup>
+
+
+	</header><!-- #masthead .site-header -->
+
+	<div id="main">
+
+
+
+<article id="post-2" class="post-2 page type-page status-publish hentry">
+	<header class="entry-header">
+		<h1 class="entry-title"><span class="contenteditable title" data-pid="2"  ><span class="saver">Sample Page</span></span></h1>
+	</header><!-- .entry-header -->
+
+
+
+
+<div class="entry-content">
+		<span class="contenteditable furniture" data-pid="2"  ><span class="saver"><p>This is an example page. It&#8217;s different from a blog post because it will stay in one place and will show up in your site navigation (in most themes).</p>
+<p>The chart below is stored in a sidebar widget that was places into the page using the <strong>Widgetize Pages Light</strong> plugin.</p>
+
+</span></span>				<span class="edit-link"><a class="post-edit-link" href="http://project.printobject.com/wp-admin/post.php?post=2&amp;action=edit" title="Edit Page">Edit</a></span>	</div><!-- .entry-content -->
+</article><!-- #post-2 -->
+
+
+
+
+
+
+	</div><!-- #main -->
+
+
+
+
+</body>
+</html>
